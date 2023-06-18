4bef04b63e7fa8fbb4aade8c0e179ec47d14ed3d
SteCard <stefanocardillo14@gmail.com>
2023-06-18 21:04:58 +0200

    markup + array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 "use strict";
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            tasks : [
+                {
+                  text: "Fare la spesa",
+                  done: false
+                },
+                {
+                  text: "Pulire la casa",
+                  done: false
+                },
+                {
+                  text: "Studiare per l'esame",
+                  done: false
+                },
+                {
+                  text: "Andare in palestra",
+                  done: false
+                },
+                {
+                  text: "Chiamare il dottore",
+                  done: false
+                },
+                {
+                  text: "Preparare la cena",
+                  done: false
+                }
+            ],
+            
+        }
+
+    },methods: {
+        taskDone(index){
+         this.tasks[index].done = !this.tasks[index].done;  
+          console.log(this.tasks[index].done)
+        }
+    },
+}).mount('#app')
---
+++ readme.md
@@ -0,0 +1,9 @@
+ESERCIZIO: Rifare l'esercizio della to do list visto a lezione. Create un array di oggetti ( con proprietà text e done )che rappresentano le task e mostratele a schermo. Aggiungete poi le funzionalità di cambio dello stato del task (cambiate il valore di done), eliminazione di una task ed aggiunta di una nuova task.
+
+1-Creo markup statico;
+
+2-Creo array oggetti;
+
+3- Ciclo array nel dom per ottenere la lista di cose da fare;
+
+4- Creo funzione per modificare la classe (se la task è stata svolta);
